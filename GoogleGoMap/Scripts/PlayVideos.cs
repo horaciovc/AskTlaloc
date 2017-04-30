6c5245d7f39f8a65a5a4e2be6a01834cdb66a85c
horaciovc <horacio.valenciac@gmail.com>
2017-04-30 16:31:15 -0700

    Assets and ProjectSettings of Unity
    Project of Unity

---
+++ GoogleGoMap/Scripts/PlayVideos.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayVideos : MonoBehaviour {
+
+	public Button SkipVideo;
+
+
+	// Use this for initialization
+	void Start () {
+
+		SkipVideo.onClick.AddListener (skipBtn);
+
+		((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+		//Handheld.PlayFullScreenMovie ("inicio.mp4", Color.black, FullScreenMovieControlMode.CancelOnInput);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	
+	}
+
+	void skipBtn(){
+
+		((MovieTexture)GetComponent<Renderer> ().material.mainTexture).Stop ();
+		GetComponent<MeshRenderer> ().enabled = false;
+
+	}
+
+}
